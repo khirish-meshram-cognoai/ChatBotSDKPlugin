393e98e42bd4655d5a8d1ac9ee2f75cf0a2ed240
Khirish7 <khirish.meshram@getcogno.ai>
2022-07-05 06:58:01 +0530

    added sdk code

---
+++ CognoChatbot-iOS/CognoChatbot-iOS/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  CognoChatbot-iOS
+//
+//  Created by Khirish Meshram on 02/05/22.
+//
+
+import Foundation
+
+public class Constants {
+    
+    public static var botUrl = ""
+    public static var botId = ""
+    public static var accessToken = ""
+    public static var category = ""
+    public static var isTokenVerify = false
+    public static let tokenVerificationUrl = "/chat/verify-access-token/"
+}
+
